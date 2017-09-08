54d166b4f12fdbbce0ba70236cd6fa5a1f8758c6
lfwang <joseph@thel.co>
2017-09-08 14:04:02 +0800

    combine ServletInitializer with SpringCacheDemoApplication

--- src/main/java/com/jospeh/demo/SpringCacheDemoApplication.java
+++ src/main/java/com/jospeh/demo/SpringCacheDemoApplication.java
@@ -2,12 +2,20 @@ package com.jospeh.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class SpringCacheDemoApplication {
+public class SpringCacheDemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 使tomcat可用
+		return builder.sources(SpringCacheDemoApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCacheDemoApplication.class, args);
 	}
